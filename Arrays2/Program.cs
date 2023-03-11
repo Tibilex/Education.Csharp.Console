ec7257aa5c8204cc6869c0d372c295558a1fc682
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-03-11 16:22:02 +0200

    Add: Education projects "Arrays"

--- Arrays2/Program.cs
+++ Arrays2/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InStep2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            const int M = 10, N = 10;
+            int[] arrA = new int[M];
+            int[] arrB = new int[N];
+            int[] arrC = new int[N];
+            int repeat = 0, index = 0;
+
+            Random rand = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                arrA[i] = rand.Next(1, 20);
+                arrB[i] = rand.Next(1, 20);
+            }
+
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                for (int j = 0; j < arrB.Length; j++)
+                {
+                    if (arrA[i] == arrB[j])
+                    {
+                        for (int k = 0; k < arrC.Length; k++)
+                        {
+                            if (arrC[k] == arrB[j])
+                                repeat++;
+                        }
+                        if (repeat == 0)
+                        {
+                            arrC[index] = arrB[j];
+                            index++;
+                        }
+                        repeat = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Массив A", Console.ForegroundColor = ConsoleColor.Yellow);
+            foreach (int i in arrA)
+                Console.Write($" {i} ");
+            Console.WriteLine();
+            Console.WriteLine("Массив B");
+            foreach (int i in arrB)
+                Console.Write($" {i} ");
+            Console.WriteLine();
+            Console.WriteLine("Массив C");
+            foreach (int i in arrC)
+                Console.Write($" {i} ");
+            Console.WriteLine();
+        }
+    }
+}
--- Arrays3/Program.cs
+++ Arrays3/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InStep2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[,] arr = new int[5, 5];
+
+            Console.WriteLine("Вся матрица 5х5\n", Console.ForegroundColor = ConsoleColor.Yellow);
+            Random rand = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rand.Next(-100, 100);
+                    Console.Write($" {arr[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            bool CheckSum = false;
+            int MaxI = 0, MinI = 0, MaxJ = 0, MinJ = 0, Sum = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if ((i == MinI && j == MinJ) || (i == MaxI && j == MaxJ))
+                    {
+                        CheckSum = !CheckSum;
+                        continue;
+                    }
+
+                    if (CheckSum)
+                        Sum += arr[i, j];
+                }
+            Console.WriteLine("\nВывод суммы между Min и Max элементами - " + Sum + "\n");
+        }
+    }
+}
+
