a5c5c91c3306864a489e129bf24848ff9582b154
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-03-11 16:13:50 +0200

    Add: education projects "Console input output"

---
+++ Console,Input.Output3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InStepSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;                                                     //Обьявляем переменую string
+            int A, B;                                                       //Обьявляем 2 переменные int
+            Console.ForegroundColor = ConsoleColor.Yellow;                  //Красим консольку =)
+            Console.WriteLine("Задача 3.\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Введите число A - ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            str = Console.ReadLine();                                       //Вводим число А
+            A = Convert.ToInt32(str);                                       //Конвертируем в целочисленый тип
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Введите число Б - ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            str = Console.ReadLine();                                       //Вводим число Б
+            Console.ForegroundColor = ConsoleColor.Green;
+            B = Convert.ToInt32(str);                                       //Конвертируем в целочисленый тип
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = A; i < B + 1; i++)                                 //Логика вывода пирамиды
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();                                              //Стоп консоли
+        }
+    }
+}
---
+++ Console.Input.Output2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InStepSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int BankAccount = 10000;                                                                            //Банковский счет 
+            int Percent, Mounth = 0;                                                                            //Процент и месяц 'Р' и 'К' по условию
+            string str;
+
+            Console.Write("Введите процент % - ", Console.ForegroundColor = ConsoleColor.Green);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            str = Console.ReadLine();
+            Percent = Convert.ToInt32(str);                                                                     //Конвертация в int
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 1; ; i++)                                                                              //Основная логика
+            {
+                if (Percent <= 0)                                                                               //Если процент 0 выход из программы
+                {
+                    Console.WriteLine("\nПроцент не может быть 0", Console.ForegroundColor = ConsoleColor.Red); //Некоректный ввод
+                    break;
+                }
+                else
+                {
+                    if (BankAccount >= 11000)                                                                   //Если счет выше 11000 Вывод данных в консоль
+                    {
+                        Console.Write("Сумма превысила 11000\n" + "Текущая сумма = ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(BankAccount);
+                        Console.Write("Количество месяцев = ", Console.ForegroundColor = ConsoleColor.Green);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(Mounth);
+                        break;
+                    }
+                    else
+                    {
+                        BankAccount = BankAccount * (100 + Percent) / 100;                                      //Считаем процент и присваеваем числу
+                        Mounth += i;                                                                            //Счетчик месяцев
+                    }
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();                                                                                  //Стоп консоли
+        }
+    }
+}
